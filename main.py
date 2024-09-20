3d83f18a949e2527b6eb53ca48ba42c46faa4f59
vieillem <matthieu.vieille-cessay@edu.esiee.fr>
2024-09-20 07:41:27 +0000

    Voici le rendu de mon code du premier exercice à envoyer ici

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+'''On importe la fonction sqrt du module math pour vérifier si le nombre cherché est premier'''
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    '''Prends en entrée un nombre entier p, et retourne si oui ou non c'est un nombre entier
+    L'utilisateur va donc choisir un nombre p que l'on va tester dans cette fonction'''
+    pr = True  #On donne de base que le nombre est premier
+    dp = round(sqrt(p)) + 1
+    for i in range(2, dp):
+        g = p % i
+        if g == 0:
+            pr = False # On met que le nombre n'est pas premier car ne respecte pas les conditions
+    if p == 1:
+        pr = False # 1 n'est pas un nombre premier
+    return pr #On retourne le résultat
 
 #### Fonction principale
 
